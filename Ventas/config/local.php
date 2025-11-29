5fc396595274f9c7d94e3ad8dcc84c09dc10f579
JCTS0714 <davidididi888@gmail.com>
2025-11-29 11:03:10 -0500

    Remove local config from repo and ignore local.php for production safety

--- Ventas/config/local.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'APP_ENV' => 'local',
-    'FORCE_HTTP' => true,
-    'BASE_HOST' => 'localhost',
-    'display_errors' => true,
-    'error_reporting' => E_ALL,
-    'FORCE_RELOAD_ON_UPDATE' => true,
-];
